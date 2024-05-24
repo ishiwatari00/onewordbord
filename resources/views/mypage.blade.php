0eeabc4ab73a03e0d3ba10258b4cf603a8ef393e
ishiwatari <k_ishiwatari@fujiball.co.jp>
2024-05-24 17:43:51 +0900

    first commit

--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -1,10 +1,18 @@
 <html>
+    <style>
+        .background{
+        background: #DCDCDC;
+        }
+        .span {
+        color: green;
+        }
+    </style>
     <head>
         <a href = "{{ url('/home') }}" >Home</a>
         {{ session('username') }}でログイン中
         <meta charset="UTF-8">
     </head>
-    <body>
+    <body class = "background">
         <header>
             <h1>自分の投稿</h1>
                 <table>
@@ -12,10 +20,12 @@
                     <form method = "get">
                     @csrf
                     <tr>
-                    <td>{{ $thread->id }}</td>
-                    <td>{{ $thread->oneword }}</td>
+                    <td>{{ $thread->id }}&emsp;名前 : <span class = "span">以下、名無しにかわりましてVIPがお送りします。</span></td>
                     <td><input type = "submit" value = "編集" formaction = "/edit"></input></td>
                     <td><input type = "submit" value = "削除" formaction = "/deletecheck"></input></td>
+                    </tr>
+                    <tr>
+                    <td>{{ $thread->oneword }}</td>
                     <td><input type = "hidden" name = "id" value = "{{ $thread->id }}"></input><td>
                     </tr>
                     </form>
